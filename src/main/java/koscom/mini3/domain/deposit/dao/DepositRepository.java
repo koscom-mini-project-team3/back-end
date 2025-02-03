5fd231a9353b669bfda726d11e984e84fcc0ab01
KangManJoo <eogns47@konkuk.ac.kr>
2025-02-03 10:43:23 +0900

    feat: add jpa repository

--- src/main/java/koscom/mini3/domain/deposit/dao/DepositRepository.java
+++ src/main/java/koscom/mini3/domain/deposit/dao/DepositRepository.java
@@ -1,4 +1,13 @@
 package koscom.mini3.domain.deposit.dao;
 
-public interface DepositRepository {
+import koscom.mini3.domain.deposit.domain.Deposit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DepositRepository extends JpaRepository<Deposit, Long> {
+    List<Deposit> findByMinContractPeriodLessThanEqualAndMaxContractPeriodGreaterThanEqualAndMinDepositLimitLessThanEqual(
+            int minTerm, int maxTerm, Long minAmount);
 }
